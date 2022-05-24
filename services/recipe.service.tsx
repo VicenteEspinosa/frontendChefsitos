419d0a9a7aff6e0c382ff7e13d8d3ab26868c48b
Vicente Espinosa <vnespinosa@uc.cl>
2022-05-24 17:03:11 -0400

    feat: added my-recipe view

--- services/recipe.service.tsx
+++ services/recipe.service.tsx
@@ -1,7 +1,19 @@
 import { NewRecipeDto } from '../dtos/recipe.dto'
 import BaseService from './base.service'
+import { BehaviorSubject } from 'rxjs'
 
 const pathPrefix = 'recipes/'
+
+export interface Recipe {
+  id: number
+  user_id: number
+  private: boolean
+  name: string
+  description: string
+  picture_url: string
+  created_at: Date
+  updated_at: Date
+}
 
 async function create(recipeData: NewRecipeDto) {
   return BaseService.request(
@@ -12,6 +24,25 @@ async function create(recipeData: NewRecipeDto) {
   )
 }
 
+async function myRecipes() {
+  return BaseService.request(
+    pathPrefix,
+    'self/',
+    BaseService.RequestMethod.Get,
+    ''
+  )
+}
+
+const recipeArraySubject = new BehaviorSubject(
+  process.browser && JSON.parse(localStorage.getItem('recipeArray') || '{}')
+)
+
 export const RecipeService = {
+  get recipeArrayValue() {
+    return Object.keys(recipeArraySubject.value).length !== 0
+      ? (recipeArraySubject.value as Recipe[])
+      : undefined
+  },
   create,
+  myRecipes,
 }
