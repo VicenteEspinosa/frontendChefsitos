a618cd2329958b2f78cd8d82e39f28973a53f10b
Rodrigo Alonso <rialonso@uc.cl>
2022-05-25 02:16:45 -0400

    chore(files): delete unnecessary example file

--- pages/api/hello.ts
+++
@@ -1,5 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next'
-
-export default function Hello(_: NextApiRequest, res: NextApiResponse) {
-  return res.status(200).json({ text: 'Hello' })
-}
