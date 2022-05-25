6711f21566ef99ce598ccd99c93f6cc7a82ee0d9
Cristian Hinostroza <cahinostroza@uc.cl>
2022-05-24 21:39:48 -0400

    feat: recipe-page created

--- services/recipe.service.tsx
+++ services/recipe.service.tsx
@@ -3,6 +3,35 @@ import BaseService from './base.service'
 
 const pathPrefix = 'recipes/'
 
+export interface Recipe {
+  id: number
+  name: string
+  picture_url: string
+  description: string
+  private: boolean
+  user_id: number
+  items: Item[]
+  ingredients: Ingredients[]
+  tags: Tag[]
+}
+
+interface Item {
+  url: string
+  body: string
+  order_number: number
+}
+
+interface Ingredients {
+  ingredient_name: string
+  measurement_name: string
+  quantity: number
+}
+
+interface Tag {
+  tag_name: string
+  tag_placeholder_url: string
+}
+
 async function create(recipeData: NewRecipeDto) {
   return BaseService.request(
     pathPrefix,
